c7c5d35abb9660217916c7ac0bf25ee59a3a5cd7
is13149 <gaal.04@mail.ru>
2022-12-20 11:07:23 +0700

    slim done

--- resources/parts/pop_category.php
+++ resources/parts/pop_category.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 col-xl-3 col-md-6">
     <div class="single_catagory">
-        <a href="jobs?cat=<?=$cat['category']?>"><h4><?=$cat['categories']?></h4></a>
+        <a href="jobs?c=<?=$cat['category']?>"><h4><?=$cat['categories']?></h4></a>
         <p><span><?=$cat['sum']?></span> Available position</p>
     </div>
 </div>
